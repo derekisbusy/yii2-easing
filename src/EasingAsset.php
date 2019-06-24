cc2dc5ce7500c82948e67cc5af1106d5fac3625a
Derek Gifford <derekisbusy@gmail.com>
2019-06-24 13:04:28 -0400

    Put sources in src folder

--- src/EasingAsset.php
+++ src/EasingAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class EasingAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/derekisbusy/yii2-easing';
+    public $sourcePath = '@vendor/derekisbusy/yii2-easing/src';
     
     public $js = [
         YII_ENV_DEV ? 'js/easing.js' : 'js/easing.min.js',
